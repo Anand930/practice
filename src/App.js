02df4be8356fd9f620baa695bda4a8be1eb5cdca
Anand930 <88427835+Anand930@users.noreply.github.com>
2024-05-23 07:20:29 +0000

    "first comit for todo"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [taskInput, setTaskInput] = useState("")
+  const [status, setStatus] = useState(false)
+  const [task, setTask] = useState([])
+  const addTask = (e) =>{
+    e.preventDefault()
+    const newArr = [...task, taskInput]
+    setTask(newArr)
+    setTaskInput("")
+  }
+
+  const handleInputChange = (e) =>{
+    setTaskInput(e.target.value)
+  }
+  
   return (
     <div className="App">
-      <h1>this is a practice repo</h1>
-    </div>
+    <h1>Todo List</h1>
+    <form onSubmit={addTask}>
+      <input
+        type="text"
+        value={taskInput}
+        onChange={handleInputChange}
+        placeholder="Add a new task"
+      />
+      <button type="submit">Add Task</button>
+    </form>
+    <ul>
+      {task.map((tasks, index) => (
+        <li key={index} className="">
+          <span >{tasks}</span>
+          <button>Remove</button>
+        </li>
+      ))}
+    </ul>
+  </div>
+
   );
 }
 
